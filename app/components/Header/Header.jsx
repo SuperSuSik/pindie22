2a4dc06cb8f0f1d74439373e6f5663f3189b6aa6
SuperSuSik <146013445+SuperSuSik@users.noreply.github.com>
2024-02-24 20:35:05 +0300

    add new changes

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,36 +1,99 @@
-import Styles from './Header.module.css';
+"use client";
+
+import { useState } from "react";
+
+import Styles from "./Header.module.css";
+import { Overlay } from "../Overlay/Overlay";
+import { Popup } from "../Popup/Popup";
+import { AuthForm } from "../AuthForm/AuthForm";
+
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 export const Header = () => {
-    return (
-      <header className={Styles['header']}>
-        <a href="./index.html" className={Styles['logo']}>
-          <img className={Styles['logo__image']} src="/images/logo.svg" alt="Логотип Pindie" />
-        </a>
-        <nav className={Styles['menu']}>
-          <ul className={Styles['menu__list']}>
-            <li className={Styles['menu__item']}>
-              <a href="" className={Styles['menu__link']}>Новинки</a>
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const [popupIsOpened, setPopupIsOpened] = useState(false);
+
+  const openPopup = () => {
+    setPopupIsOpened(true);
+  };
+  const closePopup = () => {
+    setPopupIsOpened(false);
+  };
+
+  const linkList = [
+    {
+      link: "/new",
+      title: "Новинки"
+    },
+    {
+      link: "/popular",
+      title: "Популярные"
+    },
+    {
+      link: "/shooters",
+      title: "Шутеры"
+    },
+    {
+      link: "/runners",
+      title: "Ранеры"
+    },
+    {
+      link: "/pixel-games",
+      title: "Пиксельные"
+    },
+    {
+      link: "/tds",
+      title: "TDS"
+    },
+  ];
+
+  return (
+    <header className={Styles["header"]}>
+      {pathname === "/" ? (
+        <span className={Styles["logo"]}>
+          <img
+            className={Styles["logo__image"]}
+            src="/images/logo.svg"
+            alt="Логотип Pindie"
+          />
+        </span>
+      ) : (
+        <Link href="/" className={Styles["logo"]}>
+          <img
+            className={Styles["logo__image"]}
+            src="/images/logo.svg"
+            alt="Логотип Pindie"
+          />
+        </Link>
+      )}
+      <nav className={Styles["menu"]}>
+        <ul className={Styles["menu__list"]}>
+          {linkList.map((el, index) => (
+            <li key={index.toString(36)} className={Styles["menu__item"]}>
+              <Link
+                href={el.link}
+                className={`${Styles["menu__link"]} ${
+                  pathname === el.link ? Styles["menu__link_active"] : ""
+                }`}
+              >
+                {el.title}
+              </Link>
             </li>
-            <li className={Styles['menu__item']}>
-              <a href="" className={Styles['menu__link']}>Популярные</a>
-            </li>
-            <li className={Styles['menu__item']}>
-              <a href="" className={Styles['menu__link']}>Шутеры</a>
-            </li>
-            <li className={Styles['menu__item']}>
-              <a href="" className={Styles['menu__link']}>Ранеры</a>
-            </li>
-            <li className={Styles['menu__item']}>
-              <a href="" className={Styles['menu__link']}>Пиксельные</a>
-            </li>
-            <li className={Styles['menu__item']}>
-              <a href="" className={Styles['menu__link']}>TDS</a>
-            </li>
-          </ul>
-          <div className={Styles['auth']}>
-            <button className={Styles['auth__button']}>Войти</button>
-          </div>
-        </nav>
-      </header>
-    );
+          ))}
+        </ul>
+        <div className={Styles["auth"]}>
+          <button className={Styles["auth__button"]} onClick={openPopup}>
+            Войти
+          </button>
+        </div>
+      </nav>
+      <Overlay isOpened={popupIsOpened} close={closePopup} />
+      <Popup isOpened={popupIsOpened} close={closePopup}>
+        <AuthForm />
+      </Popup>
+    </header>
+  );
 };
--- app/components/Promo/Promo.jsx
+++ app/components/Promo/Promo.jsx
@@ -1,6 +1,29 @@
+"use client";
+import { useState, useEffect } from "react";
+
+
 import Styles from "./Promo.module.css";
 
 export const Promo = () => {
+  const [codeIsVisible, setCodeIsVisible] = useState(false);
+
+  const handleButtonCLick = () => {
+    if (!codeIsVisible) {
+      setCodeIsVisible(true)
+    }
+  };
+  useEffect(() => {
+    let timeout;
+    if (codeIsVisible) {
+        timeout = setTimeout(() => {
+            setCodeIsVisible(false);
+        }, 5000);    
+    }
+    return () => {
+        clearTimeout(timeout);
+    };
+  }, [codeIsVisible]);
+
   return (
     <section className={Styles['promo']}>
       <div className={Styles['promo__description-block']}>
@@ -8,7 +31,15 @@ export const Promo = () => {
         <p className={Styles['promo__description']}>
           Скидка на все курсы Яндекс Практикума для пользователей нашего сайта!
         </p>
-        <button className={`button ${Styles['promo__button']}`}>Получить код</button>
+        <button className={`button ${Styles['promo__button']}`}
+        onClick = {handleButtonCLick}
+      >
+        {codeIsVisible ? (
+          <span className={Styles["Promo-code"]}>WEBTEENS10</span>
+        ) : (
+          "Получить код"
+        )}
+        </button>
       </div>
       <img
         src="/images/promo-illustration.svg"
